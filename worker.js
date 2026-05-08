24f6b21ec931342c51e7b9aef82995cafeca897e
republikz <gabriel.goes1998@gmail.com>
2026-05-08 12:21:46 -0300

    Bump dashboard label patch version

--- worker.js
+++ worker.js
@@ -3,7 +3,7 @@ import { onRequest as handleHealthRequest } from "./functions/api/health.js";
 
 const PATCH_TAGS = [
   '<script src="/persistence-patch.js?v=3" defer></script>',
-  '<script src="/dashboard-settings-patch.js?v=2" defer></script>',
+  '<script src="/dashboard-settings-patch.js?v=3" defer></script>',
   '<script src="/sidebar-brand-patch.js?v=3" defer></script>',
   '<script src="/emerald-icon-patch.js?v=4" defer></script>'
 ];
