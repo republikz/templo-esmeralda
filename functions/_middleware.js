1857cf5e2eda2f4917a433fcadc00bd70da7b5d5
republikz <gabriel.goes1998@gmail.com>
2026-05-09 11:19:43 -0300

    Load recurring finance automation middleware patch

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -2,7 +2,8 @@ const PATCH_TAGS = [
   '<script src="/persistence-patch.js?v=3" defer></script>',
   '<script src="/dashboard-settings-patch.js?v=3" defer></script>',
   '<script src="/sidebar-brand-patch.js?v=3" defer></script>',
-  '<script src="/emerald-icon-patch.js?v=4" defer></script>'
+  '<script src="/emerald-icon-patch.js?v=4" defer></script>',
+  '<script src="/recurring-finance-patch.js?v=1" defer></script>'
 ];
 
 function injectPatches(html) {
